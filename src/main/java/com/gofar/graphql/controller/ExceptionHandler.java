0f0b95dcf40b7d4c9d3834079b9d3b49296aceaf
Tountoun <tountounabela@gmail.com>
2024-02-29 19:25:21 +0000

    Handle author expection and rename exception handler

--- src/main/java/com/gofar/graphql/controller/ExceptionHandler.java
+++ src/main/java/com/gofar/graphql/controller/ExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.gofar.graphql.controller;
 
+import com.gofar.graphql.exception.AuthorException;
 import com.gofar.graphql.exception.BookException;
 import graphql.GraphQLError;
 import graphql.schema.DataFetchingEnvironment;
@@ -7,12 +8,20 @@ import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter
 import org.springframework.stereotype.Component;
 
 @Component
-public class HandleException extends DataFetcherExceptionResolverAdapter {
+public class ExceptionHandler extends DataFetcherExceptionResolverAdapter {
     @Override
     protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
         if (ex instanceof BookException) {
             return GraphQLError.newError()
                     .errorType(((BookException)ex).getErrorType())
+                    .message(ex.getMessage())
+                    .path(env.getExecutionStepInfo().getPath())
+                    .location(env.getField().getSourceLocation())
+                    .build();
+        }
+        if (ex instanceof AuthorException) {
+            return GraphQLError.newError()
+                    .errorType(((AuthorException)ex).getErrorType())
                     .message(ex.getMessage())
                     .path(env.getExecutionStepInfo().getPath())
                     .location(env.getField().getSourceLocation())
